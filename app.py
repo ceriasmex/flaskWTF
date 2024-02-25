8d57af15481def62f12c2ad08a549469e7a14b6c
ceriasmex <ceriasmex@gmail.com>
2024-02-25 17:15:06 -0600

    Chapter it is update and running.

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+bootstrap = Bootstrap(app)
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
 class NameForm(FlaskForm):
-    name = StringField('Whats is ypur name?', validators=[DataRequired()])
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Solo se que no se nada'
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'))
